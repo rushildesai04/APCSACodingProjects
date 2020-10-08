21f107fd9816064e0170b514578d68bbafbd7432
Rushil D <66389126+rush-bot@users.noreply.github.com>
2020-10-07 22:37:06 -0700

    Add files via upload

---
+++ Welcome.java
@@ -0,0 +1,16 @@
+/*
+ *Welcome to the MOOC! 
+ */
+
+public class Welcome{
+  public static void main(String[] args){
+  
+    System.out.println("This is Rushil Desai");
+  
+    System.out.println("Also known as:");
+    
+    System.out.println("The GOAT");
+  
+  
+  }
+}
